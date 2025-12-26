bb1f813f17c85cb2f87b371cbe871d567301dd2e
VARUN-SINGH-LATWAL <varunsingh0456@gmail.com>
2025-12-26 19:23:43 +0530

    Initial Express + TypeScript setup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express,{Application, Request, Response} from "express";
+
+const app: Application = express();
+
+const PORT: number = 3000 ;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Express + TypeScript Server 🚀" });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
